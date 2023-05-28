89e28f8b195c5461f4519deed0b94e4826cf6366
Pearlynn <98682935+PearlynnT@users.noreply.github.com>
2023-05-28 15:16:52 +0800

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+const BASE_URL = 'https://task-master-evop.onrender.com';
+
 export default axios.create({
-    baseURL: 'http://localhost:3500'
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
 });
