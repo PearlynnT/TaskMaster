6ce4e50d7681ad47871ecf27df7f6b4fec66506e
PearlynnT <pearlynntoh@gmail.com>
2023-06-26 03:45:18 +0800

    post api test

--- backend/__tests__/api.test.js
+++ backend/__tests__/api.test.js
@@ -48,6 +48,13 @@ describe("GET /users/", () => {
     }, 200000);
 });
 
+describe("GET /users/ no authentication", () => {
+    it("should return unauthorised response", async () => {
+        const res = await api.get("/users");
+        expect(res.statusCode).toBe(401);
+    });
+});
+
 describe("GET /users/:id", () => {
     it("should return a user", async () => {
         const token = await api.post("/login").send({
@@ -78,7 +85,31 @@ describe("GET /projects/", () => {
                 "Content-Type": "application/json"
             });
         expect(res.statusCode).toBe(200);
-        expect(res.body.length).toBe(0); // may be changed, currently number of projects==0
+        expect(res.body.length).toBeGreaterThan(0); // may be changed
+    }, 200000);
+});
+
+describe("POST /projects/", () => {
+    it("should create a project", async () => {
+        const token = await api.post("/login").send({
+            user: "Eric",
+            pwd: "Testing123!"
+        });
+        const res = await api
+            .post("/projects/")
+            .send({
+                own: "6493e6e1f861f3986f5df50c",
+                name: "Test Project",
+                description: "This is a test.",
+                memb: ["6493e6e8f861f3986f5df50f"],
+                completed: false
+            })
+            .set({
+                Authorization: "Bearer " + token._body.accessToken,
+                "Content-Type": "application/json"
+            });
+        expect(res.statusCode).toBe(201);
+        expect(res.body.name).toBe("Test Project");
     }, 200000);
 });
 
@@ -97,4 +128,30 @@ describe("GET /tasks/", () => {
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBeGreaterThan(0); // may be changed
     }, 200000);
+});
+
+describe("POST /tasks/", () => {
+    it("should create a task", async () => {
+        const token = await api.post("/login").send({
+            user: "Eric",
+            pwd: "Testing123!"
+        });
+        const res = await api
+            .post("/tasks/")
+            .send({
+                proj: "64989566792b2bb8de2335d4",
+                name: "Task 1",
+                description: "This is the first task.",
+                priority: "high",
+                assigned: "6493e6e1f861f3986f5df50c",
+                date: new Date(),
+                completed: false
+            })
+            .set({
+                Authorization: "Bearer " + token._body.accessToken,
+                "Content-Type": "application/json"
+            });
+        expect(res.statusCode).toBe(201);
+        expect(res.body.name).toBe("Task 1");
+    });
 });
--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+// not used
+
 const express = require('express');
 const app = express();
 const path = require('path');
