104ef391ecdd0ea3311939a1e3d8786ff9263a61
EricSimWP <110527948+EricSimWP@users.noreply.github.com>
2023-07-03 15:08:29 +0800

    Added Delete Conformation Modal

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import '../style/project.css';
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import deleteIcon from '../icon/delete.png'
+import deleteIcon from '../icon/delete.png';
+import DeleteConformationModal from './DeleteConformationModal';
 
 function Project(props) {
     const axiosPrivate = useAxiosPrivate();
     const toggle = props.toggle;
+    const [deleteToggle, setDeleteToggle] = useState(false);
 
-    const handleDelete = async (id) => {
+    const handleDelete = () => {
+        setDeleteToggle(true);
+    }
+
+    const onCancel = () => {
+        setDeleteToggle(false);
+    }
+
+    const onConfirm = async (id) => {
         try {
             const response = await axiosPrivate.delete(
                 `/projects/${id}`
@@ -15,20 +25,27 @@ function Project(props) {
         } catch (err) {
             console.log(err);
         }
+        setDeleteToggle(false);
     }
 
     return (
-        <div className="project">
-            <div className="project--header">
-                <h3 className='project--name'>{props.name}</h3>
-                <h4 className='project--member'>{props.members.length + 1 === 1 ? '1 member' : props.members.length + 1 + ' members'}</h4>
-                <button className='project--button' onClick={() => handleDelete(props._id)}>
-                    <img className='project--delete' src={deleteIcon} alt='Delete Button' />
-                </button>
+        <div>
+            <div className="project">
+                <div className="project--header">
+                    <h3 className='project--name'>{props.name}</h3>
+                    <h4 className='project--member'>{props.members.length + 1 === 1 ? '1 member' : props.members.length + 1 + ' members'}</h4>
+                    <div className='project--actions'>
+                        <button className='project--button' onClick={handleDelete}>
+                            <img className='project--delete' src={deleteIcon} alt='Delete Button' />
+                        </button>
+                    </div>
+                </div>
+                <div className='divider'></div>
+                <p className='project--description'>{props.description}</p>
             </div>
-            <div className='divider'></div>
-            <p className='project--description'>{props.description}</p>
+            <DeleteConformationModal isOpen={deleteToggle} onCancel={onCancel} onConfirm={() => onConfirm(props._id)} />
         </div>
+        
     )
 }
 
