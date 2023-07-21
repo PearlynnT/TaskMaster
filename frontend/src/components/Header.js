a7eff9d629d5e1eb379972f7e12398e3a252bfab
EricSimWP <110527948+EricSimWP@users.noreply.github.com>
2023-07-21 12:44:35 +0800

    Added Notification Bell

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Logout from "./Logout"
 import '../style/header.css';
 import Profile from "./Profile";
+import bellIcon from "../icon/bell.png"; 
+import Notification from './Notification';
 
 function Header() {
+    const [toggle, setToggle] = useState(false);
+
+    const handleBellClick = () => {
+        setToggle(!toggle);
+    }
+
     return (
         <div className="header--container">
             <Profile />
             <h1 className="header--title">Task Master</h1>
+            <img 
+                src={bellIcon} 
+                alt='Bell Icon' 
+                className="header--bell"
+                onClick={handleBellClick}
+            />
             <Logout className="header--logout"/>
+            {toggle && <Notification />}
         </div>
     )
 }
---
+++ frontend/src/components/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/notification.css';
+
+function Notification () {
+    return (
+        <div className="notification--container">
+            You have no notifications
+        </div>
+    )
+}
+
+export default Notification;
